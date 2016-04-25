0de26f8e134d7d44e9a79cb741db363aff87ea0e
Gilberto Junior <gjunior@tray.net.br>
2016-04-25 18:10:15 +0000

    Corrigindo SQL

--- script.sql
+++ script.sql
@@ -7,14 +7,25 @@
 -- Versão do servidor: 5.7.11
 -- PHP Version: 5.6.19
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+  time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
+/*!40101 SET
+  @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+
+/*!40101 SET
+  @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+
+/*!40101 SET
+  @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+
+/*!40101 SET NAMES
+  utf8mb4 */;
+
 
 --
 -- Database: `estoque`
@@ -26,13 +37,14 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `estoques`
 --
 
-CREATE TABLE `estoques` (
-  `id` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `produto_id` int(11) NOT NULL,
-  `quantidade` int(11) NOT NULL DEFAULT '0',
-  `criado_em` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `estoques`(
+  `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `usuario_id` INT(11) NOT NULL,
+  `produto_id` INT(11) NOT NULL,
+  `quantidade` INT(11) NOT NULL DEFAULT '0',
+  `criado_em` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
 
 -- --------------------------------------------------------
 
@@ -40,11 +52,12 @@ CREATE TABLE `estoques` (
 -- Estrutura da tabela `produtos`
 --
 
-CREATE TABLE `produtos` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `quantidade` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `produtos`(
+  `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `quantidade` INT(11) NOT NULL DEFAULT '0'
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
 
 
 -- --------------------------------------------------------
@@ -53,13 +66,14 @@ CREATE TABLE `produtos` (
 -- Estrutura da tabela `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `senha` varchar(255) DEFAULT NULL,
-  `perfil` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `usuarios`(
+  `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `senha` VARCHAR(255) DEFAULT NULL,
+  `perfil` VARCHAR(10) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
 
 --
 -- Indexes for dumped tables
@@ -68,24 +82,27 @@ CREATE TABLE `usuarios` (
 --
 -- Indexes for table `estoques`
 --
-ALTER TABLE `estoques`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usuario_id` (`usuario_id`),
-  ADD KEY `produto_id` (`produto_id`);
+ALTER TABLE
+  `estoques` 
+  ADD KEY `usuario_id`(`usuario_id`),
+  ADD KEY `produto_id`(`produto_id`);
+
 
 --
 -- Indexes for table `produtos`
 --
-ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `nome` (`nome`);
+ALTER TABLE
+  `produtos` 
+  ADD KEY `nome`(`nome`);
+
 
 --
 -- Indexes for table `usuarios`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE
+  `usuarios` 
+  ADD UNIQUE KEY `email`(`email`);
+
 
 --
 -- Constraints for dumped tables
@@ -94,10 +111,16 @@ ALTER TABLE `usuarios`
 --
 -- Limitadores para a tabela `estoques`
 --
-ALTER TABLE `estoques`
-  ADD CONSTRAINT `estoques_produto_id_foreign` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`id`),
-  ADD CONSTRAINT `estoques_usuario_id_foreign` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+ALTER TABLE
+  `estoques` ADD CONSTRAINT `estoques_produto_id_foreign` FOREIGN KEY(`produto_id`) REFERENCES `produtos`(`id`),
+  ADD CONSTRAINT `estoques_usuario_id_foreign` FOREIGN KEY(`usuario_id`) REFERENCES `usuarios`(`id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+/*!40101 SET
+  CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+
+/*!40101 SET
+  CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+
+/*!40101 SET
+  COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
